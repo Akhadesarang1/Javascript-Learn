e4a0d1408285b4f71532293af668a0ac5c260d6f
Sarang Akhade <Akhadesarang1@gmail.com>
2026-02-07 17:06:35 +0530

    Update variable declarations in Variables.js
    Changed variable declaration for accountState to use 'let'.

--- 01-Basics/Variables.js
+++ 01-Basics/Variables.js
@@ -2,7 +2,7 @@ const accountId = 1039 // due to const it cant be changed
 let accountEmail = "sarangakhade@prisms.in" // due to let you can reassign but cannot redeclare
 var accountPassword = '12345' // oldest method but it gets messy with scopes
 accountCity = 'Pune' // this can be done but never prefer this type of declaration
-let accountState // we havent declared any value so o/p is undefined
+let accountState // we havent declared any value so o/p is undefined 
 // accountId = 2 //not allowed
 accountEmail = 'akhadesarang1@gmail.com'
 accountPassword = '543'
